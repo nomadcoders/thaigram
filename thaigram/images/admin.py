d0d9234ec8cdde476f5b08838c4930ff8e5fb214
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-06 16:54:15 +0700

    Django Admin

--- thaigram/images/admin.py
+++ thaigram/images/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import Image
 
-# Register your models here.
+
+@admin.register(Image)
+class AdminImage(admin.ModelAdmin):
+    
+    list_display = (
+        'caption',
+        'location',
+        'created_at',
+        'updated_at'
+    )
+
+    list_filter = (
+        'location',
+        'created_at'
+    )
+
+    search_fields = (
+        'location',
+    )
+
+    ordering = (
+        'location',
+    )
+
+
--- thaigram/images/models.py
+++ thaigram/images/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Image(models.Model):
+
+    file = models.FileField()
+    caption = models.TextField()
+    location = models.CharField(max_length=50, default='')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.caption} - {self.location}'

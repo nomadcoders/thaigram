a26b6e947a4b388523f97ce65526184e0e492979
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-07 16:27:58 +0700

    Finished templates

--- thaigram/images/views.py
+++ thaigram/images/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Comment, Image, Like
+  
 
 # Create your views here.
 def index_view(potato):
     if potato.method == "GET":
         if potato.user.is_authenticated:
-            user = potato.user
             return render(potato, 'feed.html')
         else:
             return render(potato, 'login.html')

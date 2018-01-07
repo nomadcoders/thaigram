82e7ec5e093f90ad118e876b24d669f7c3b45b57
Nicolás Serrano Arévalo <itnico.las.me@gmail.com>
2018-01-07 10:31:13 +0700

    Index view with Auth Checking

--- thaigram/images/views.py
+++ thaigram/images/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index_view(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return HttpResponse(f'Your email is {request.user.email}')
+        else:
+            return HttpResponse('Please log in.')
